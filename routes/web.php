e5ee399f50ae111b7a0bc9bae51632d63504f990
Andrei Telteu <andrei@telteu.ro>
2026-08-14 19:35:27 +0300

    Show live monitor at root

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,11 @@
 
 use App\Http\Controllers\LiveDeviceController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/', [LiveDeviceController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('home');
 
 Route::get('/dashboard', [LiveDeviceController::class, 'index'])
     ->middleware(['auth', 'verified'])
